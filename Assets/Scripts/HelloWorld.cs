4f5d79e3af284ed454a918bcbda967745eac993f
thing01 <elliotaeustis@gmail.com>
2022-01-17 06:51:49 -0500

    Changed Hello World to Hello Developing World.

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -8,7 +8,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("Hello Developing World");
     }
 
     // Update is called once per frame
